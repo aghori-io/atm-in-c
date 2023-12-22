d60623c6f75e4109a5392484f75e3d6205dcac10
aghori-io <153995209+aghori-io@users.noreply.github.com>
2023-12-22 14:53:26 +0545

    Update ATM Machine.c
    Added comments on the program

--- ATM Machine.c
+++ ATM Machine.c
@@ -1,9 +1,11 @@
+//This is a simple ATM machine program in C.
+
 #include<stdio.h>
-#define PIN 1234
+#define PIN 1234 //default pincode for the ATM
 
 int main() {
-	char proceed;
-	int inpPin, re;
+	char proceed; //variable proceed takes a single key entry as sign to imitate the card has been inserted
+	int inpPin, re; //inpPin variable takes user input pincode
 	float amt, wdrawAmt;
 	
 	amt = 9999.99;
@@ -13,7 +15,7 @@ int main() {
 	printf("Enter your pin: \a");
 	scanf("%d", &inpPin);
 	
-	if(inpPin==PIN){
+	if(inpPin==PIN){ //program proceeds if the user input pin matches the predefined pincode, else it restarts from pinEntry
 		amtEntry:
 		printf("Enter the amount you want to withdraw: \a");
 		scanf("%2f", &wdrawAmt);
@@ -23,14 +25,14 @@ int main() {
 			goto amtEntry;
 		}
 			else {
-				amt = amt - wdrawAmt;
+				amt = amt - wdrawAmt; //calculation of remaining balance in the account
 				printf("________________\n");
-					printf("Please Collect Your Cash \a\nLast Withdraw Amt: %.2f \n", wdrawAmt);
-					printf("Remaining Balance: %.2f \n", amt);
+					printf("Please Collect Your Cash \a\nLast Withdraw Amt: %.2f \n", wdrawAmt); 
+					printf("Remaining Balance: %.2f \n", amt); //limiting amount to 2 decimal points 
 					printf("________________\nThank You");
-					printf("Withdraw Again? \t 0. No 1. Yes \n");
-					fflush(stdin);
-					scanf("%d", &re);
+					printf("Withdraw Again? \t 0. No 1. Yes \n"); //confirmation for rewithdrawal - if the user wants to withdraw amount again, the program goes to pinEntry
+					fflush(stdin); //buffer clearance
+					scanf("%d", &re); 
 					if(re==1){
 						goto pinEntry;
 					}
@@ -52,4 +54,3 @@ int main() {
 	}
 	
 	
-
